e1c30b65163e5bde4ce723ff2f7cecd94491c3bb
liuluosheng <346642458@qq.com>
2018-09-01 23:00:28 +0800

    0901

--- src/app/sys/identity/users/users.component.ts
+++ src/app/sys/identity/users/users.component.ts
@@ -6,7 +6,7 @@ import { environment } from '../../../../environments/environment';
 import { NzModalService, NzNotificationService } from 'ng-zorro-antd';
 import { SFComponent } from '@delon/form';
 import { compare } from 'fast-json-patch';
-import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -27,6 +27,7 @@ export class UsersComponent implements OnInit {
   sortValue: string = null;
   sortMap = {};
   cloumns = [];
+  filters = [];
   showFilter = false;
   @ViewChild(SFComponent)
   private sf: SFComponent;
@@ -51,7 +52,43 @@ export class UsersComponent implements OnInit {
     });
     this.refreshStatus();
   }
-
+  hideFilter(): void {
+    this.showFilter = false;
+    this.filters = [];
+    this.getpage(this.pageindex, this.pagesize);
+  }
+  filter(odataProperty: any, op: OdataOperard, value: string): void {
+    const lastFilterString = this.filters.map((o) => o.value ).join(` ${OdataOperard.And} `);
+    this.filters = this.filters.filter((n) => n.name !== odataProperty.name || ( n.name === odataProperty.name && n.operard !== op));
+    if (value) {
+     switch (op) {
+      case OdataOperard.Equals:
+      case OdataOperard.GreaterThan:
+      case OdataOperard.GreaterThanOrEqual:
+      case OdataOperard.LessThan:
+      case OdataOperard.LessThanOrEqual:
+      case OdataOperard.NotEquals:
+      this.filters = [...this.filters, {
+        name: odataProperty.name,
+        operard: op,
+        value: odataProperty.enum ?
+        `${odataProperty.name} ${op} '${value}'` :
+        `${odataProperty.name} ${op} ${value}`}];
+      break;
+      case OdataOperard.Contains:
+      this.filters = [...this.filters, {
+        name: odataProperty.name,
+        operard: op,
+        value: `${op}(${odataProperty.name}, '${value}')`}];
+      break;
+     }
+    }
+    const filterString = this.filters.map((o) => o.value ).join(` ${OdataOperard.And} `);
+    if (lastFilterString !== filterString  ) {
+    this.getpage(this.pageindex, this.pagesize, null, filterString );
+    }
+     console.log(filterString);
+  }
   sort(sortName: string, value: string): void {
     this.sortName = sortName;
     this.sortValue = value;
@@ -99,9 +136,9 @@ export class UsersComponent implements OnInit {
       }
     });
   }
-  getpage(pageindex: number, pagesize: number, sort?: string) {
+  getpage(pageindex: number, pagesize: number, sort?: string, filter?: string) {
     this.loading = true;
-    this.service.Page(pageindex, pagesize, sort || 'CreatedDate').subscribe((o) => {
+    this.service.Page(pageindex, pagesize, sort || 'CreatedDate', filter).subscribe((o) => {
       this.dataSet = o.data;
       this.total = o.count;
       this.loading = false;
@@ -118,4 +155,17 @@ export class UsersComponent implements OnInit {
     this.getpage(this.pageindex, this.pagesize);
   }
 
+}
+
+enum OdataOperard {
+  Contains = 'contains',
+  Equals = 'eq',
+  GreaterThan = 'gt',
+  GreaterThanOrEqual= 'ge',
+  NotEquals = 'ne',
+  LessThan = 'lt',
+  LessThanOrEqual = 'le',
+  And= 'and',
+  Or= 'or',
+  Not= 'not'
 }
--- src/shared/services/injectable/ODataQueryService.ts
+++ src/shared/services/injectable/ODataQueryService.ts
@@ -8,7 +8,6 @@ import { EntityBase } from '../dto/EntityBase';
 export class ODataQueryService<T extends EntityBase> {
     private odata: ODataService<T>;
     constructor(private oDataService: ODataServiceFactory) {
-
     }
     public init(type: string) {
         this.odata = this.oDataService.CreateService<T>(type);
@@ -19,15 +18,19 @@ export class ODataQueryService<T extends EntityBase> {
             first<T>()
         );
     }
-    public Page(pageindex: number, pagesize: number, orderby: string): Observable<ODataPagedResult<T>> {
-        return this.odata.Query().Skip((pageindex - 1) * pagesize).Top(pagesize).OrderBy(orderby).ExecWithCount();
+    public Page(pageindex: number, pagesize: number, orderby: string, filter?: string): Observable<ODataPagedResult<T>> {
+        return this.odata.Query()
+        .Skip((pageindex - 1) * pagesize)
+        .Filter(filter)
+        .Top(pagesize)
+        .OrderBy(orderby)
+        .ExecWithCount();
     }
     public Create(item: T): Observable<T> {
         return this.odata.Post(item);
     }
     public Update(item: T): Observable<T> {
         return this.odata.Put<T>(item, item.Id);
-
     }
     public UpdateByPatch(entity: any, key: any): Observable<HttpResponse<T>> {
         return this.odata.Patch(entity, key);
