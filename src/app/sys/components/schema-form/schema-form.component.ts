363ea25ca0143ead07b32bfe167b7f4395a4777e
liuluosheng <346642458@qq.com>
2018-09-11 22:19:42 +0800

    0911

--- src/app/sys/components/schema-form/schema-form.component.ts
+++ src/app/sys/components/schema-form/schema-form.component.ts
@@ -1,27 +1,56 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, Input, SimpleChange, OnChanges } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-schema-form',
   templateUrl: './schema-form.component.html',
   styleUrls: ['./schema-form.component.less']
 })
-export class SchemaFormComponent implements OnInit {
+export class SchemaFormComponent implements OnChanges {
+  layout: 'horizontal'|'vertical' = 'vertical';
+  names: any[];
+  @Input()
+   schema: any;
 
   @Input()
-  schema: any = { properties: {} };
-  validateForm: FormGroup;
+  data: any = {};
+
+
+
+  validateForm: FormGroup = new FormGroup({});
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  submit(value) {
+  console.log(value);
+  }
+  initValidateForm() {
     const controls: any = {};
-    for (const key in this.schema.properties) {
-      if (this.schema.properties.hasOwnProperty(key)) {
-        const element = this.schema.properties[key];
-
+    this.names = Object.keys(this.schema.properties);
+    this.names.forEach((key) => {
+      let validators = [];
+      if (this.schema.required.findIndex((v) => v === key) !== -1) {
+        validators = [...validators, Validators.required];
       }
-    }
+      if (this.schema.properties[key].pattern) {
+        validators = [...validators, Validators.pattern(this.schema.properties[key].pattern)];
+      }
+      controls[key] = [this.data[key], validators];
+    });
     this.validateForm = this.fb.group(controls);
+    console.log(this.validateForm);
+  }
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    const schemaIndex = Object.keys(changes).findIndex((key) => key === 'schema');
+    if (schemaIndex !== -1 && changes['schema'].previousValue != null) {
+      this.initValidateForm();
+    }
+   }
   }
 
-}
+
+
