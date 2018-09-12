2f464c15940b25e45059567c155a330ff5d11c1c
346642458@qq.com <346642458@qq.com>
2018-09-12 14:29:43 +0800

    0912

--- src/app/sys/components/schema-form/schema-form.component.ts
+++ src/app/sys/components/schema-form/schema-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, SimpleChange, OnChanges } from '@angular/core';
 import {
-  AbstractControl,
   FormBuilder,
   FormGroup,
   Validators
@@ -12,26 +11,31 @@ import {
   styleUrls: ['./schema-form.component.less']
 })
 export class SchemaFormComponent implements OnInit {
-  layout: 'horizontal'|'vertical' = 'vertical';
-  _schema: any;
+  layout: 'horizontal' | 'vertical' = 'vertical';
+  cols: 24 | 12 | 8 = 12; // 布局的列数： 24表示1列，12表示2列，8表示3列
+  private _schema: any;
   names: any[];
 
   @Input()
   set schema(value: any) {
     if (value) {
-    this._schema = value;
-    this.initValidateForm();
+      this._schema = value;
+      this.initValidateForm();
     }
   }
-
+  get schema() {
+    if (this._schema) {
+      return Object.keys(this._schema.properties).map((value) => this._schema.properties[value]);
+    }
+  }
   @Input()
-   data: any = {};
+  data: any = {};
 
   validateForm: FormGroup = new FormGroup({});
   constructor(private fb: FormBuilder) { }
 
-  submit(value) {
-  console.log(value);
+  submit(event) {
+    console.log(this.validateForm.value);
   }
   initValidateForm() {
     const controls: any = {};
@@ -51,8 +55,8 @@ export class SchemaFormComponent implements OnInit {
   }
   ngOnInit() {
 
-   }
   }
+}
 
 
 
