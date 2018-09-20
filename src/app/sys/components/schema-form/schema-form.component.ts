cdf5dd83b312e99e809ebc1900be4b014042e7a6
liuluosheng <346642458@qq.com>
2018-09-20 21:30:03 +0800

    upload

--- src/app/sys/components/schema-form/schema-form.component.ts
+++ src/app/sys/components/schema-form/schema-form.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input, SimpleChange, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange, OnChanges, Output, EventEmitter } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   Validators
 } from '@angular/forms';
+import { deepClone } from 'fast-json-patch/lib/core';
+import { environment } from '../../../../environments/environment';
+
 
 @Component({
   selector: 'app-schema-form',
@@ -15,6 +18,8 @@ export class SchemaFormComponent implements OnInit, OnChanges {
   cols: 24 | 12 | 8 = 12; // 布局的列数： 24表示1列，12表示2列，8表示3列
   private _schema: any[];
   private _data: any;
+  uploadFileList?: any = {};
+  validateForm: FormGroup = new FormGroup({});
   @Input()
   set schema(value: any) {
     if (value) {
@@ -24,7 +29,12 @@ export class SchemaFormComponent implements OnInit, OnChanges {
   get schema() {
     if (this._schema) {
       return {
-        text: this._schema.filter((n) => n.type === 'string' || n.type === 'enum' || n.type === 'number' || n.type === 'datetime'),
+        text: this._schema.filter((n) =>
+        n.type === 'string' ||
+        n.type === 'enum' ||
+        n.type === 'number' ||
+        n.type === 'boolean' ||
+        n.type === 'datetime'),
         upload: this._schema.filter((n) => n.type === 'upload')
       };
     }
@@ -37,19 +47,22 @@ export class SchemaFormComponent implements OnInit, OnChanges {
   get formData() {
     return this._data || {};
   }
-  validateForm: FormGroup = new FormGroup({});
+  @Output() formSumbit = new EventEmitter<any>();
   constructor(private fb: FormBuilder) { }
-
-  submit(event) {
+  /// 表单提交
+  submit() {
     Object.keys(this.validateForm.controls).forEach((i) => {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     });
-    console.log(this.validateForm.value);
+    if (this.validateForm.valid) {
+     this.formSumbit.emit(Object.assign(deepClone(this.formData), this.validateForm.value));
+    }
   }
   reset() {
     this.validateForm.reset();
   }
+  /// 初始化一个FormGroup
   initValidateForm(schema?: any) {
     const controls: any = {};
     const _props = schema ? Object.keys(schema.properties).map((value) => schema.properties[value]) : this._schema;
@@ -72,11 +85,34 @@ export class SchemaFormComponent implements OnInit, OnChanges {
     this.validateForm = this.fb.group(controls);
     this._schema = _props;
   }
+  /// 更新表单项目值
   updateFormData(item: any) {
     Object.keys(this.validateForm.controls).forEach((key) => {
       this.validateForm.controls[key].setValue(item[key]);
     });
   }
+  /// 更新编辑表单时上传控件的文件list
+  /// 与api的filescontroller配合使用
+  fileUploadList() {
+    if (this.schema.upload) {
+      this.schema.upload.forEach((v) => {
+        const _value = this.validateForm.controls[v.name].value;
+        const reg = /(^|&)?name=([^&]*)(&|$)/;
+        if (_value && _value.search(reg) !== -1) {
+            this.uploadFileList[v.name] = [{
+              name: _value.match(reg)[0].split('=')[1],
+              url: `${environment.apiUrl}/${_value}`,
+              response: {
+                result: _value
+              }
+            }];
+         } else {
+          this.uploadFileList[v.name] = [];
+         }
+      });
+    }
+  }
+  /// 文件上传控件的状态更改触发事件
   fileUpload(event, schema): void {
     const control = this.validateForm.controls[schema.name];
     if (event.type === 'success') {
@@ -101,6 +137,7 @@ export class SchemaFormComponent implements OnInit, OnChanges {
     Object.keys(changes).forEach((key) => {
       if (key === 'formData' && changes[key].currentValue) {
         this.updateFormData(changes[key].currentValue);
+        this.fileUploadList();
       }
     });
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
+  apiUrl: 'http://localhost:8000',
   oDataBaseUrl: 'http://localhost:8000/odata',
   jsonSchemaUrl: 'http://localhost:8000/api/jsonschema',
+  // 可以自动上传token的url
   oauthSendTokenUrl: ['http://localhost:8000/api', 'http://localhost:8000/odata']
 };
 
